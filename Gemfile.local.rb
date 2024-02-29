dc8a471ba81bbba1d7da7183d0ce73241f998d9a
Jon <jonathon.lockwood@networkedweb.com>
2024-03-01 00:33:49 +0930

    feat: proxmox plugin
    !1

---
+++ Gemfile.local.rb
@@ -0,0 +1,9 @@
+# not needed as it can be integrated directly with AWX
+# https://github.com/theforeman/foreman_ansible
+# gem 'foreman_ansible'
+gem 'foreman_fog_proxmox'
+# Not needed https://community.theforeman.org/t/external-ipam-core-feature-merged/17871
+# https://github.com/grizzthedj/foreman_ipam
+# gem 'foreman_ipam'
+# https://github.com/theforeman/foreman_discovery
+# gem 'foreman_discovery'
